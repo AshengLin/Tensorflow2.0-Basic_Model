a4a31111c416bee5e761bc462622a4ed2e61b93e
Derek <a0988238060@gmail.com>
2021-10-03 21:01:10 +0800

    CNN_1003

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+import os
+
+
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train = x_train/255
 x_test = x_test/255
 # print(x_train.shape)  # (60000, 28, 28)
 x_train = x_train.reshape((60000, 28, 28, 1))  # 灰階
 
-from tensorflow import keras
-from tensorflow.keras import layers
-import os
 
 cnn_model = keras.Sequential(name='CNN')
 cnn_model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
@@ -30,3 +33,6 @@ cnn_model.fit(x_train, y_train, epochs=10)
 if os.path.exists('model.png'):
     os.remove('model.png')
 keras.utils.plot_model(cnn_model, show_shapes='True', to_file='model.png')
+
+print(np.mean(cnn_model.predict_classes(x_test.reshape((-1, 28, 28, 1))) == y_test))
+
